2b36b32797fa99cdea4bd6f3efe7c2c42108c909
malinitesh-sketch <malinitesh29022008@email.com>
2026-05-10 12:52:42 +0530

    Test sub one

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Traveloop Backend Running"
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+
+    username = data.get("username")
+    password = data.get("password")
+
+    if username == "admin" and password == "1234":
+        return jsonify({
+            "success": True,
+            "message": "Login successful"
+        })
+
+    return jsonify({
+        "success": False,
+        "message": "Invalid credentials"
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
