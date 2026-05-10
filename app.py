f46a692653fa0477d0ecf59608f6c06c848a3f4e
malinitesh-sketch <malinitesh29022008@email.com>
2026-05-10 13:10:56 +0530

    Test sub oneee

--- app.py
+++ app.py
@@ -2,22 +2,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/login", methods=["POST"])
-def login():
+@app.route("/register", methods=["POST"])
+def register():
     data = request.json
 
-    username = data.get("username")
-    password = data.get("password")
-
-    if username == "admin" and password == "1234":
-        return jsonify({
-            "success": True,
-            "message": "Login successful"
-        })
+    print(data)
 
     return jsonify({
-        "success": False,
-        "message": "Invalid credentials"
+        "success": True,
+        "message": "User registered"
     })
 
 if __name__ == "__main__":
